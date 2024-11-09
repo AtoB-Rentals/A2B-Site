8ba770352e613c8e2c379149b55576486ba27325
Emmanuel <emmanuelbrown1310@gmail.com>
2024-11-08 20:21:34 -0500

    Manager_site: prod push 3

--- src/app/(manager)/manager/cars/page.tsx
+++ src/app/(manager)/manager/cars/page.tsx
@@ -1,18 +1,19 @@
 "use client"
+import ListCars from "@/components/manager/cars/listCars"
 // import ListCars from "@/components/manager/cars/listCars"
 import dynamic from "next/dynamic"
 
-const DynamicComponentWithNoSSR = dynamic(
-  () => import('@/components/manager/cars/listCars'),
-  { ssr: false }
-)
+// const DynamicComponentWithNoSSR = dynamic(
+//   () => import('@/components/manager/cars/listCars'),
+//   { ssr: false }
+// )
 
 const ManagerCars = () => {
     
 
     return (
         <main>
-            <DynamicComponentWithNoSSR />
+            <ListCars />
         </main>
     )
 }
